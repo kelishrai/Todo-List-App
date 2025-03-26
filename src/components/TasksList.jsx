ac838d9228811e24addbefe7c907052f55a9fe81
Kelish Rai <raikelish7@gmail.com>
2025-03-26 11:57:02 +0545

    design and functionality completed as provided by v0

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,26 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiDeleteBinLine } from "react-icons/ri";
 
-const Task = ({ task }) => {
+const Task = ({ task, handleActiveClick, handleCompletedClick }) => {
+    const { todoId, description, active, completed } = task;
+
     return (
         <li className="task">
             <div>
-                <input type="checkbox" />
-                {task}
+                <input
+                    type="checkbox"
+                    checked={active}
+                    onChange={() => handleActiveClick(todoId)}
+                />
+                <span className={active ? "crossed" : ""}>{description}</span>
             </div>
-            <RiDeleteBinLine />
+            <RiDeleteBinLine onClick={() => handleCompletedClick(todoId)} />
         </li>
     );
 };
 
-const TasksList = ({ tasks, status }) => {
-    return (
-        <ul className="tasks mt">
-            {tasks.map((task) => (
-                <Task task={task} />
-            ))}
-        </ul>
-    );
+const TasksList = ({
+    tasks,
+    status,
+    handleActiveClick,
+    handleCompletedClick,
+}) => {
+    const selectedTab = () => {
+        switch (status) {
+            case "All":
+                return tasks.map(
+                    (task) =>
+                        task.completed == false && (
+                            <Task
+                                key={task.todoId}
+                                task={task}
+                                handleActiveClick={handleActiveClick}
+                                handleCompletedClick={handleCompletedClick}
+                            />
+                        )
+                );
+
+            case "Active":
+                return tasks.map(
+                    (task) =>
+                        task.active == true &&
+                        task.completed == false && (
+                            <Task
+                                key={task.todoId}
+                                task={task}
+                                handleActiveClick={handleActiveClick}
+                                handleCompletedClick={handleCompletedClick}
+                            />
+                        )
+                );
+
+            case "Completed":
+                return tasks.map(
+                    (task) =>
+                        task.completed == true && (
+                            <Task
+                                key={task.todoId}
+                                task={task}
+                                handleActiveClick={handleActiveClick}
+                                handleCompletedClick={handleCompletedClick}
+                            />
+                        )
+                );
+        }
+    };
+
+    return <ul className="tasks">{selectedTab()}</ul>;
 };
 
 export default TasksList;
